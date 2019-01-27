6d5a68a65abff0ad535c5634ed54f4bdd3dc78f0
Marek Majerski <marek.majerski@gmail.com>
2019-01-28 00:22:02 +0100

    remove additional options to pick other systems

--- features/step_definitions/tester_work_steps.rb
+++ features/step_definitions/tester_work_steps.rb
@@ -5,14 +5,16 @@ Given /^I open (Your Devices) from drop down menu$/ do |menu_option|
   tester_work.open_menu_option(menu_option)
 end
 
-Given /^I add (Windows 10) to operating system$/ do
+Then /^I add Windows 10 to operating system$/ do
   # we can add parameter to add any system by string
   your_devices = YourDevicesPage.new
+  your_devices.wait_until_add_another_desktop_device_button_visible
   your_devices.add_desktop_device
 end
 
-Given /^I remove (Windows 10) from operating system$/ do
+Then /^I remove Windows 10 from operating system$/ do
   # we can add parameter to remove any system by string
   your_devices = YourDevicesPage.new
+  your_devices.wait_until_add_another_desktop_device_button_visible
   your_devices.remove_desktop_device
 end
--- features/support/pages/your_devices_page.rb
+++ features/support/pages/your_devices_page.rb
@@ -3,21 +3,22 @@ require 'site_prism'
 class YourDevicesPage < SitePrism::Page
   set_url 'https://testers.testerwork.com/tester-account/profile/hardware-software'
 
-  element :add_another_desktop_device, :xpath, '//button[text()=" Add another desktop device"]'
+  element :add_another_desktop_device_button, :xpath, '//button[text()=" Add another desktop device"]'
   element :open_drop_menu, :xpath, '//div[@class="Select-control"]'
   # for quick solution i added only one system, but it can be changed to get paremeter
-  element :select_windows_10, :xpath, "//div[@class='Select-menu']/div[@class='Select-option'][text()='Windows 10']"
-  element :remove_windows_10, :xpath, "//div[@class='Select-menu']/div[@class='Select-option'][text()='Windows 10']"
+  element :select_windows_10_option, :xpath, "//div[@class='Select-menu']/div[@class='Select-option'][text()='Windows 10']"
+  element :remove_windows_10, :xpath, "//strong[text()='Windows 10']/../../..//button[@type='submit']"
   element :save_button, :xpath, "//button[@type='submit'][text()='Save']"
 
   def add_desktop_device
-    add_another_desktop_device.click
+    add_another_desktop_device_button.click
     open_drop_menu.click
-    select_windows_10.click
+    select_windows_10_option.click
     save_button.click
   end
 
   def remove_desktop_device
     remove_windows_10.click
+    save_button.click
   end
 end
